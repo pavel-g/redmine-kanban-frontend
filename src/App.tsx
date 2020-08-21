d31b1000f7d1faff04b80488da2ba27437afc72d
Gnedov Pavel <pavel.gnedov@eltex.loc>
2020-08-21 09:20:38 +0700

    Added redux

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 import {AppBar, IconButton, Toolbar, Typography} from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-
-// TODO: 2020-08-20 Сделать начальную страницу
+import {Provider} from "react-redux";
+import store from "./redux/store";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -24,18 +24,20 @@ function App() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Redmine Kanban
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Provider store={store}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Redmine Kanban
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </Provider>
   )
 }
 
