3629ca8bf6a8c6e91c33446acb8e34d66dd8936e
pavel.gnedov <pavel.gnedov@eltex.loc>
2020-09-10 11:54:47 +0700

    Updated board styles

--- src/components/kanban.tsx
+++ src/components/kanban.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import {KanbanConfig} from "../models/jkanban/kanban-config";
 import ReactTrello from "react-trello";
 import {ConverterJKanbanConfigToTrelloConfig} from "../converters/converter-jkanban-config-to-trello-config";
+import {observer} from "mobx-react";
 
-function Kanban(props: {id: number, config: KanbanConfig}) {
+const Kanban = observer((props: {id: number, config: KanbanConfig}) => {
   const data: ReactTrello.BoardData = ConverterJKanbanConfigToTrelloConfig(props.config);
   return (
-    <ReactTrello data={data}></ReactTrello>
+    <ReactTrello style={{backgroundColor: "white", overflowY: "revert", height: "revert"}} data={data}></ReactTrello>
   )
-}
+})
 
 export default Kanban
