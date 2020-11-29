0c58df9c929684de6b7a46ab25205191171d494c
Gnedov Pavel <pavel@gnedov.info>
2020-11-29 22:15:23 +0700

    Card title click handler added

--- src/components/custom-card.tsx
+++ src/components/custom-card.tsx
@@ -3,6 +3,7 @@ import {CustomCardModel} from "../models/custom-card-model";
 import {observer} from "mobx-react";
 import {MovableCardWrapper, CardHeader, CardTitle, CardRightContent, Detail, Footer} from "../styles/Base";
 import {CustomCardMergerequestsField} from "./custom-card-mergerequests-field";
+import {Config} from "../config";
 
 export const CustomCard = observer((props: CustomCardModel) => {
   if (!props) {
@@ -15,10 +16,16 @@ export const CustomCard = observer((props: CustomCardModel) => {
 
   const metadata = props.metadata
 
+  const issueUrl = `${Config.redminePublicUrl}/issues/${metadata.issueNumber}`
+
+  const onTitleClick = () => {
+    window.open(issueUrl)
+  }
+
   return (
     <MovableCardWrapper>
       <CardHeader>
-        <CardTitle>{props.title}</CardTitle>
+        <CardTitle onClick={onTitleClick}>{props.title}</CardTitle>
         <CardRightContent></CardRightContent>
       </CardHeader>
       <Detail>
