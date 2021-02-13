711bc0f9c8a0a6fe81e2e598e8a7e7fad9a03bea
pavel.gnedov <pavel.gnedov@eltex.loc>
2021-02-13 17:04:12 +0700

    Saving card settings in local storage added

--- src/store/custom-card-settings-store.ts
+++ src/store/custom-card-settings-store.ts
@@ -17,24 +17,47 @@ export class CustomCardSettingsStore {
 
   setDescription(value: boolean): void {
     this.settings.description = value
+    this.saveSettings()
   }
 
   setUsers(value: CustomCardSettingsUsersViewOption): void {
     this.settings.users = value
+    this.saveSettings()
   }
 
   setMergeRequests(value: boolean): void {
     this.settings.mergeRequests = value
+    this.saveSettings()
   }
 
   setProgress(value: boolean): void {
     this.settings.progress = value
+    this.saveSettings()
   }
 
   setSpentTime(value: boolean): void {
     this.settings.spentTime = value
+    this.saveSettings()
+  }
+
+  setSettings(value: CustomCardSettingsModel): void {
+    this.settings.description = value.description
+    this.settings.spentTime = value.spentTime
+    this.settings.progress = value.progress
+    this.settings.users = value.users
+    this.settings.mergeRequests = value.mergeRequests
+  }
+
+  private saveSettings(): void {
+    const settings = JSON.stringify(this.settings)
+    window.localStorage.setItem('card_settings', settings)
   }
 
 }
 
 export const customCardSettingsStore = new CustomCardSettingsStore(DefaultCustomCardSettingsConst)
+
+const settings = window.localStorage.getItem('card_settings')
+if (typeof settings === 'string') {
+  customCardSettingsStore.setSettings(JSON.parse(settings))
+}
