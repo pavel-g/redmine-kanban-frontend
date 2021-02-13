e564d83f910d453cf5f0aff2ce316ca7c6703aeb
pavel.gnedov <pavel.gnedov@eltex.loc>
2021-02-13 15:38:47 +0700

    Acceptence arguments in function "delay" added

--- src/function/delay.ts
+++ src/function/delay.ts
@@ -1,9 +1,11 @@
-export function delay(callback: (...args: any[]) => void, ms: number = 0): () => void {
+export function delay(callback: (...args: any[]) => void, ms: number = 0): (...args: any[]) => void {
   let timer: number|null = null
-  return () => {
+  let argsStore: any[]
+  return (...args: any[]) => {
+    argsStore = args
     if (timer !== null) {
       clearTimeout(timer)
     }
-    timer = setTimeout(callback, ms)
+    timer = setTimeout(callback(...argsStore), ms)
   }
 }
